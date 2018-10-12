a436819d23f12f1153f551d8c4611796222bfee2
Thomas VERVIALLE <ThomasV@acelys.local>
2018-10-12 17:45:20 +0200

    uj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AcelysLunchTime';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyCTAnI8AiYmSrPhmF4nGUcHb9gXprUfBhk",
+      authDomain: "acelyslunchtime.firebaseapp.com",
+      databaseURL: "https://acelyslunchtime.firebaseio.com",
+      projectId: "acelyslunchtime",
+      storageBucket: "acelyslunchtime.appspot.com",
+      messagingSenderId: "837469563532"
+    };
+    firebase.initializeApp(config);
+
+  }
 }
